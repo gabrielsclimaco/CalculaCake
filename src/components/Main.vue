6d8f4a06995b66609dfb25dda17945e697b50fd3
gabrielsclimaco <gabrielsclimaco@gmail.com>
2018-01-04 16:04:48 -0200

    🎨 Set up UI

---
+++ src/components/Main.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+ q-layout(ref='layout', view='lHh Lpr fff', :left-class="{'bg-grey-2': true}")
+  q-toolbar(slot='header').toolbar
+    q-toolbar-title.toolbar-title
+      img(src='../statics/logo.png').logo
+      span.title-label CalculaCake
+  div
+    router-view
+</template>
+
+<script>
+import {
+  QLayout,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon
+} from 'quasar'
+
+export default {
+  components: {
+    QLayout,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style>
+.logo {
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+}
+.toolbar-title {
+  display: flex;
+  align-items: end;
+}
+.title-label {
+  font-size: 1.5em;
+}
+.toolbar {
+  background: linear-gradient(45deg, #ddd6f3, #faaca8);
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -25,7 +25,12 @@ export default new VueRouter({
   scrollBehavior: () => ({ y: 0 }),
 
   routes: [
-    { path: '/', component: load('Hello') },
+    {
+      path: '/',
+      component: load('Main'),
+      children: [
+      ]
+    },
 
     // Always leave this last one
     { path: '*', component: load('Error404') } // Not found
